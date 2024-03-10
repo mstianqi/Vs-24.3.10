ca32c4a0e8e361e2615c1c77de509fd660ed11c1
Tianqi <Tian@qi.com>
2024-03-10 19:57:06 +0800

    修改1

--- 24-3-10-进阶/test.c
+++ 24-3-10-进阶/test.c
@@ -1,18 +1,33 @@
 ﻿#include <stdio.h>
 
 //枚举
-enum day
+//enum day
+//{
+//	Mon,
+//	Tues,
+//	Wen,
+//	Thur,
+//	Fri,
+//	Sat,
+//	Sun
+//};
+//int main()
+//{
+//	enum day d = Fri;
+//	return 0;
+//}
+
+//联合体
+union un
 {
-	Mon,
-	Tues,
-	Wen,
-	Thur,
-	Fri,
-	Sat,
-	Sun
+	int a;
+	char c;
 };
 int main()
 {
-	enum day d = Fri;
-	return 0;
+	union un u;
+	printf("%p\n", &u);
+	printf("%p\n", &u.a);
+	printf("%p\n", &u.c);
+	return;
 }
